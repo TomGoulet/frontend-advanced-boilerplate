e4afb39cd76babba7f583b1e1aa2241593c4363e
Thomas Goulet <t.gouletpro@gmail.com>
2017-02-27 14:59:26 +0100

    finished home and team page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,5 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
